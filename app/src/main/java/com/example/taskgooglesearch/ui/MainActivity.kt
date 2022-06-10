494aff91f4506b372af9bcd5f83eee63826cca55
vemir <vemir.amirkhanyan@gmail.com>
2022-06-10 10:30:57 +0400

    Added Recycler View

--- app/src/main/java/com/example/taskgooglesearch/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskgooglesearch/ui/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.taskgooglesearch.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.data.model.Response
 import com.example.taskgooglesearch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +16,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     val viewModel: SearchViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var recAdapter: SearchRecAdapter
+    private lateinit var recyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,9 +25,8 @@ class MainActivity : AppCompatActivity() {
         observeModelLiveData()
         observemodelLiveDataState()
         val searchView = binding.searchView
-        val recyclerView = binding.recyclerView
         searchView.clearFocus()
-        recyclerView.layoutManager = LinearLayoutManager(this)
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let { viewModel.searchData(it) }
@@ -42,9 +43,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun observeModelLiveData() {
         viewModel.modelLiveData.observe(this) { result ->
-            for (searchResult in result.searchResults) {
-                Log.e("TAG", searchResult.title)
+            val tempResList = result.searchResults
+            recAdapter = SearchRecAdapter(tempResList){
+                Toast.makeText(this, "go brouser", Toast.LENGTH_SHORT).show()
             }
+            recyclerView = binding.recyclerView
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = recAdapter
         }
     }
     private fun observemodelLiveDataState() {
---
+++ app/src/main/java/com/example/taskgooglesearch/ui/SearchRecAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.taskgooglesearch.ui
+
+import android.view.LayoutInflater
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskgooglesearch.databinding.SearchItemBinding
+import com.example.taskgooglesearch.domain.models.SearchResult
+
+class SearchRecAdapter(
+    private val searchResultlList: List<SearchResult>,
+    private val onItemClick: (webSiteLink: String) -> Unit
+
+) : RecyclerView.Adapter<SearchRecAdapter.SearchViewHolder>() {
+
+
+    override fun getItemCount(): Int = searchResultlList.size
+
+    inner class SearchViewHolder(val binding: SearchItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val tvLink = binding.tvLink
+        val tvTitle = binding.tvTitle
+        val tvDescription = binding.tvDescription
+
+        fun bind(searchResult: SearchResult) {
+            tvLink.text = searchResult.link
+            tvTitle.text = searchResult.title
+            tvDescription.text = searchResult.description
+
+            tvTitle.setOnClickListener {
+                if (tvLink.text.toString().isNotEmpty()) {
+                    onItemClick.invoke(tvLink.text.toString())
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder =
+        SearchViewHolder(
+            SearchItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        val tempSearchModel = searchResultlList.get(position)
+        holder.bind(tempSearchModel)
+    }
+}
