c63afe967f38a6bff5c1460dddf68ceff628be2c
vemir <vemir.amirkhanyan@gmail.com>
2022-06-10 09:15:07 +0400

    Added searchVIew

--- app/src/main/java/com/example/taskgooglesearch/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskgooglesearch/ui/MainActivity.kt
@@ -3,26 +3,51 @@ package com.example.taskgooglesearch.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.data.model.Response
 import com.example.taskgooglesearch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    val viewModel: SearchViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val viewModel: SearchViewModel by viewModels()
+        observeModelLiveData()
+        observemodelLiveDataState()
+        val searchView = binding.searchView
+        val recyclerView = binding.recyclerView
+        searchView.clearFocus()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { viewModel.searchData(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+
+        })
+
+    }
+
+    private fun observeModelLiveData() {
         viewModel.modelLiveData.observe(this) { result ->
             for (searchResult in result.searchResults) {
                 Log.e("TAG", searchResult.title)
             }
         }
+    }
+    private fun observemodelLiveDataState() {
         viewModel.modelLiveDataState.observe(this) { response ->
             when (response) {
                 is Response.Loading -> binding.loading.visibility = View.VISIBLE
@@ -31,9 +56,6 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, response.message, Toast.LENGTH_SHORT).show()
                 }
             }
-        }
-        binding.button.setOnClickListener {
-            viewModel.searchData("Valod")
         }
     }
 }
